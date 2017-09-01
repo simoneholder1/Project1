66bd8c431ff9513edd9d88c7c6f568b048ef8cfa
Simone Holder <simoneholder@Simones-MacBook-Air.local>
2017-09-01 15:36:41 -0700

    third update

---
+++ src/Components/RestaurantDisplay.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+import getResaurants from '../APIs/YelpAPI';
+
+export default class RestaurantDisplay extends Component{
+    constructor(){
+        super();
+        this.state={
+        finalRestaurant: "",
+    }
+
+}}
